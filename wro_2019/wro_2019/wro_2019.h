9759d07b321241c1775589059d07edb6a4f38684
Mikhail.kenny5660 <misha25_live@mail.ru>
2019-01-30 23:39:47 +1000

    initial commit

---
+++ wro_2019/wro_2019/wro_2019.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int wro_2019Test();
+
+#ifdef __cplusplus
+}
+#endif
---
+++ wro_2019/wro_2019_exe/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "wro_2019.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	char sz[] = "Hello, World!";	//Hover mouse over "sz" while debugging to see its contents
+	cout << sz << endl;	//<================= Put a breakpoint here
+	return 0;
+}
