7f5b5467a202d55b5f714c42973b5941787f7d63
Mikhail.kenny5660 <misha25_live@mail.ru>
2019-06-16 19:43:29 +1000

    add imread

--- wro2019main/Robot_high_lvl_lib/VisionAlgs.cpp
+++ wro2019main/Robot_high_lvl_lib/VisionAlgs.cpp
@@ -2,6 +2,10 @@
 #include "debug.h"
 box_color_t VisionGetSmallBox(const cv::Mat& frame)
 {
+
+	
+	#if !(defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__))
 	save_debug_img("SmallBox.jpg", frame);
+	#endif
 	return undefined_bc;
 }
--- wro2019main/Wro_Vision_Opencv/Wro_Vision_Opencv/Wro_Vision_Opencv.cpp
+++ wro2019main/Wro_Vision_Opencv/Wro_Vision_Opencv/Wro_Vision_Opencv.cpp
@@ -1,11 +1,13 @@
-﻿// Wro_Vision_Opencv.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// Wro_Vision_Opencv.cpp : Этот файл содержит функцию "main". Здесь начинается и
+// заканчивается выполнение программы.
 //
 
-#include "pch.h"
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n"; 
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+const std::string path_small_cube("C:\\Users\\misha\\YandexDisk\\OneDrive\\Robots\\NImyrio\\WRO2019\\smallBox\\");
+int main() {
+  cv::Mat img;
+  img = cv::imread(path_small_cube + "OLL2.jpg");
+  std::cout << "Hello World!\n";
 }
-
