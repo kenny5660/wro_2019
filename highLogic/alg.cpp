e4fdaee52ce51609464c3721dee493b5df0f9329
DanilaG <dndv279@gmail.com>
2019-06-18 23:15:10 +1000

    Change хуйню.

--- highLogic/alg.cpp
+++ highLogic/alg.cpp
@@ -15,16 +15,16 @@ Point catch_flower_offset[4] = {
     {-robot_sett::catch_flower_offset, 0},
     {0, robot_sett::catch_flower_offset},
     {robot_sett::catch_flower_offset, 0},
-	{0, -robot_sett::catch_flower_offset}
+    {0, -robot_sett::catch_flower_offset}
 };
 
 //TODO: анализ после мёртвой зоны
 
 int turn2box(Robot &robot, BoxMap &box, Map &map) {
     int need_rot = 1;
-    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-        show_debug_img("", map.get_img());
-    #endif
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    show_debug_img("", map.get_img());
+#endif
     Point buff_p = box.get_box_indent();
     buff_p = buff_p - Point{field_sett::size_field_unit, field_sett::size_field_unit};
     if (fabs(buff_p.get_x() - box.get_left_corner_point().get_x()) < fabs(buff_p.get_y() - box.get_left_corner_point().get_y())) {
@@ -45,9 +45,9 @@ int turn2box(Robot &robot, BoxMap &box, Map &map) {
     robot.Turn(need_rot_rad);
     map.set_new_position(RobotPoint{map.get_position().get_x(), map.get_position().get_y(),
                                     -need_rot_rad +  map.get_position().get_angle()});
-    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-        show_debug_img("", map.get_img());
-    #endif
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    show_debug_img("", map.get_img());
+#endif
     return need_rot;
 }
 
@@ -79,42 +79,42 @@ void go2box(Robot &robot, std::vector<Point> way, Robot::CatchCubeSideEnum side_
 }
 
 void frame_connect(Robot &robot, double out_way_offset, double start_angle) {
-	robot.Turn(-start_angle);
-	Point point_offset = { -field_sett::parking_zone_door_size / 2., out_way_offset };
-	std::vector<PolarPoint> lidar_dt;
-	robot.GetLidarPolarPoints(lidar_dt);
-	auto points = get_corners(lidar_dt);
-	std::pair<int, int> ind_nearly_point = { 0, 0 };
-	for (int i = 1; i < points.size(); i++) {
-		for (int j = 0; j < points[i].size(); j++) {
-			if (points[ind_nearly_point.first][ind_nearly_point.second].dist(point_offset) >
-			    points[i][j].dist(point_offset)) {
-				ind_nearly_point = std::make_pair(i, j);
-			}
-		}
-	}
-	{
-		DebugFieldMat mat;
-		auto ln = line2line_type(points);
-		add_lines_img(mat, ln);
-		add_point_img(mat);
-		add_point_img(mat, point_offset);
-		save_debug_img("frame_conect", mat);
-	}
-	Point p = { points[ind_nearly_point.first][ind_nearly_point.second].get_y(), -points[ind_nearly_point.first][ind_nearly_point.second].get_x() - field_sett::parking_zone_door_size / 2.0 };
-	robot.Go2({ { 0, p.get_y() } });
-	robot.Go2({ { p.get_x() + 110, 0 } });
-	robot.AlliginByDist(45, 0);
+    robot.Turn(-start_angle);
+    Point point_offset = { -field_sett::parking_zone_door_size / 2., out_way_offset };
+    std::vector<PolarPoint> lidar_dt;
+    robot.GetLidarPolarPoints(lidar_dt);
+    auto points = get_corners(lidar_dt);
+    std::pair<int, int> ind_nearly_point = { 0, 0 };
+    for (int i = 1; i < points.size(); i++) {
+        for (int j = 0; j < points[i].size(); j++) {
+            if (points[ind_nearly_point.first][ind_nearly_point.second].dist(point_offset) >
+                points[i][j].dist(point_offset)) {
+                ind_nearly_point = std::make_pair(i, j);
+            }
+        }
+    }
+    {
+        DebugFieldMat mat;
+        auto ln = line2line_type(points);
+        add_lines_img(mat, ln);
+        add_point_img(mat);
+        add_point_img(mat, point_offset);
+        save_debug_img("frame_conect", mat);
+    }
+    Point p = { points[ind_nearly_point.first][ind_nearly_point.second].get_y(), -points[ind_nearly_point.first][ind_nearly_point.second].get_x() - field_sett::parking_zone_door_size / 2.0 };
+    robot.Go2({ { 0, p.get_y() } });
+    robot.Go2({ { p.get_x() + 110, 0 } });
+    robot.AlliginByDist(45, 0);
 }
 
 Point go_from_frame(Robot &robot, double dist, double ang) {
     robot.Go2({{0, dist}});
-	robot.Turn(ang);
+    robot.Turn(ang);
     return {cos(ang) * dist, sin(ang) * dist};
 }
 
 color_t do_box(Robot &robot, Map &map, BoxMap &box, Robot::CatchCubeSideEnum &side_catch,
-                bool need_next, bool kamikaze_mode, show_img_debug db) {
+               bool need_next, bool kamikaze_mode, show_img_debug db) {
     std::vector<Point> way;
     int need_rot = turn2box(robot, box, map);
     {
@@ -173,12 +173,12 @@ color_t do_box(Robot &robot, Map &map, BoxMap &box, Robot::CatchCubeSideEnum &si
 
 void do_alg_code(Robot &robot, bool kamikaze_mode, std::string s) {
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-	clear_logs();
+    clear_logs();
 #endif
     Robot::CatchCubeSideEnum side_catch = Robot::CatchCubeSideEnum::LEFT;
     cv::Mat QRCodeImg;
     if (s == "") {
-        robot.GetQRCode(QRCodeImg);
+        robot.WayFromFrame(QRCodeImg);
     }
     std::array<BoxMap, 3> boxes;
     std::pair<Point, Point> pz;
@@ -205,21 +205,21 @@ void do_alg_code(Robot &robot, bool kamikaze_mode, std::string s) {
     start_position.set_angle(start_angle);
     save_debug_img("QRCodeDetection", map.get_img());
     std::vector<Point> way;
-	show_img_debug db;	
-	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-		db = show_debug_img;
-	#else
-		db = save_debug_img;
-	#endif
-	for (auto i : boxes) {
+    show_img_debug db;
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    db = show_debug_img;
+#else
+    db = save_debug_img;
+#endif
+    for (auto i : boxes) {
         do_box(robot, map, i, side_catch, false, kamikaze_mode, db);
     }
     way.clear();
     Point b;
-	robot.Turn(map.get_position().get_angle() - M_PI_2);
-	auto buff = map.get_position();
-	buff.add_angle(-map.get_position().get_angle() + M_PI_2);
-	map.set_new_position(buff);
+    robot.Turn(map.get_position().get_angle() - M_PI_2);
+    auto buff = map.get_position();
+    buff.add_angle(-map.get_position().get_angle() + M_PI_2);
+    map.set_new_position(buff);
     if(!go_to2(map, start_position, way, b, kamikaze_mode, db))
         return;
     robot.Go2(way);
@@ -229,31 +229,41 @@ void do_alg_code(Robot &robot, bool kamikaze_mode, std::string s) {
 RobotPoint detect_position(Robot &robot, std::vector<PolarPoint> &lidar_data, double frame_offset) {
     //TODO: проверить, что стойки не мешают алгоритму.
     show_img_debug debug;
-    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-        debug = show_debug_img;
-    #else
-        debug = save_debug_img;
-    #endif
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    debug = show_debug_img;
+#else
+    debug = save_debug_img;
+#endif
 
+    robot.GetLidarPolarPoints(lidar_data);
     auto lines = get_corners(lidar_data);
     double min_length = 2 * field_sett::size_field_unit;
 
     // Нужно развернуть систему координат.
     double ang = get_angle_lines(lines,
-        {{frame_offset, field_sett::parking_zone_door_size / 2.},
-         {frame_offset, -field_sett::parking_zone_door_size / 2.}},
-         2 * field_sett::size_field_unit);
-    corners_rot(lines, ang);
+                                 {
+                                     { frame_offset, field_sett::parking_zone_door_size / 2. },
+                                     { frame_offset, -field_sett::parking_zone_door_size / 2. }
+                                 },
+                                 2 * field_sett::size_field_unit);
+    {
+        DebugFieldMat mat;
+        add_lines_img(mat, lines);
+        debug("befor_rot", mat);
+    }
+    corners_rot(lines, -ang);
     {
         DebugFieldMat mat;
         add_lines_img(mat, lines);
         debug("Init_robot_from_start", mat);
     }
 
-    std::array<std::pair<int, int>, 4> extra_line = {std::make_pair(-1, 0),
-                                                     std::make_pair(-1, 0),
-                                                     std::make_pair(-1, 0),
-                                                     std::make_pair(-1, 0)};
+    std::array<std::pair<int, int>, 4> extra_line = {
+        std::make_pair(-1, 0),
+        std::make_pair(-1, 0),
+        std::make_pair(-1, 0),
+        std::make_pair(-1, 0)
+    };
     for (int i = 0; i < lines.size(); i++) {
         for (int j = 0; j < (lines[i].size() - 1); j++) {
             if (lines[i][j].dist(lines[i][j + 1]) > (min_length)) {
@@ -262,13 +272,16 @@ RobotPoint detect_position(Robot &robot, std::vector<PolarPoint> &lidar_data, do
                     fabs(lines[i][j].get_y() - lines[i][j + 1].get_y())) {
                     if (lines[i][j].get_y() > 0) {
                         line_type = top_field_margin;
-                    } else {
+                    }
+                    else {
                         line_type = bottom_field_margin;
                     }
-                } else {
+                }
+                else {
                     if (lines[i][j].get_x() > 0) {
                         line_type = right_field_margin;
-                    } else {
+                    }
+                    else {
                         line_type = left_field_margin;
                     }
                 }
@@ -276,7 +289,7 @@ RobotPoint detect_position(Robot &robot, std::vector<PolarPoint> &lidar_data, do
                     dist_line2point(
                         lines[extra_line[line_type].first][extra_line[line_type].second],
                         lines[extra_line[line_type].first][extra_line[line_type].second + 1],
-                        {0, 0}) < dist_line2point(lines[i][j], lines[i][j + 1], {0, 0})) {
+                        { 0, 0 }) < dist_line2point(lines[i][j], lines[i][j + 1], { 0, 0 })) {
                     extra_line[line_type].first = i;
                     extra_line[line_type].second = j;
                 }
@@ -287,13 +300,23 @@ RobotPoint detect_position(Robot &robot, std::vector<PolarPoint> &lidar_data, do
     std::vector<std::pair<int, PolarPoint>> suspicious_points;
     for (int i = 0; i < extra_line.size(); i++) {
         if (extra_line[i].first >= 0) {
-            suspicious_points.emplace_back(i, PolarPoint(dist_line2point(lines[extra_line[i].first][extra_line[i].second],
-                                                                             lines[extra_line[i].first][extra_line[i].second + 1],
-                                                                             Point{0, 0}),
-                                                             get_middle_line_ang(lines[extra_line[i].first][extra_line[i].second],
-                                                                                 lines[extra_line[i].first][extra_line[i].second + 1],
-                                                                                 Point{0, 0})));
+            suspicious_points.emplace_back(i,
+                                           PolarPoint(Point{(lines[extra_line[i].first][extra_line[i].second].get_x() + lines[extra_line[i].first][extra_line[i].second  + 1].get_x()) / 2.0, (lines[extra_line[i].first][extra_line[i].second].get_y() + lines[extra_line[i].first][extra_line[i].second  + 1].get_y()) / 2.0}.dist(),
+
+                                                      get_middle_line_ang(lines[extra_line[i].first][extra_line[i].second],
+                                                                          lines[extra_line[i].first][extra_line[i].second + 1],
+                                                                          Point{ 0, 0 }) + ang));
         }
+    }
+    {
+        DebugFieldMat mat;
+        add_lines_img(mat, lines);
+        for (int i = 0; i < suspicious_points.size(); i++) {
+            PolarPoint p = suspicious_points[i].second;
+            p.add_f(-ang);
+            add_point_img(mat, p.to_cartesian());
+        }
+        debug("Init_robot_from_start", mat);
     }
     if (suspicious_points.size() > 1) {
         std::vector<std::pair<int, color_t>> colors = robot.GetColorFromAng(suspicious_points);
@@ -302,12 +325,15 @@ RobotPoint detect_position(Robot &robot, std::vector<PolarPoint> &lidar_data, do
                 std::cerr << "Misha, BLIA. It might need sort!!!" << std::endl;
                 continue;
             }
-            RobotPoint p = dist2coordinates(
-                dist_line2point(lines[extra_line[colors[i].first].first][extra_line[colors[i].first].second],
-                                lines[extra_line[colors[i].first].first][extra_line[colors[i].first].second + 1],
-                                {0, 0}),
-                i);
-            pos.merge(p);
+            if (colors[i].second == black_c)
+            {
+                RobotPoint p = dist2coordinates(
+                    dist_line2point(lines[extra_line[colors[i].first].first][extra_line[colors[i].first].second],
+                                    lines[extra_line[colors[i].first].first][extra_line[colors[i].first].second + 1],
+                                    { 0, 0 }),
+                    i);
+                pos.merge(p);
+            }
         }
     }
     pos.set_angle(ang);
@@ -321,7 +347,7 @@ void update_box_color(Robot &robot, Map &map) {
         if(boxes[i].get_color() == undefined_c) {
             Point box = boxes[i].get_left_corner_point() + field_sett::climate_box_width / 2.;
             double ang = atan2(box.get_y() - map.get_position().get_y(),
-                box.get_x() - map.get_position().get_x());
+                               box.get_x() - map.get_position().get_x());
             points.emplace_back(i, PolarPoint{boxes[i].get_left_corner_point().dist(map.get_position()),
                                               ang});
         }
@@ -329,7 +355,7 @@ void update_box_color(Robot &robot, Map &map) {
     auto colors_box = robot.GetColorFromAng(points);
     for (int i = 0; i < colors_box.size(); i++) {
         if ((colors_box[i].second != black_c) && (colors_box[i].second != undefined_c) && (colors_box[i].second != white_c)) {
-                map.set_box_color(colors_box[i].first, colors_box[i].second);
+            map.set_box_color(colors_box[i].first, colors_box[i].second);
         } else {
             std::cout << "Undefined box " << colors_box[i].first << std::endl;
         }
@@ -349,11 +375,12 @@ bool get_box(color_t color, Map &map, BoxMap &box) {
 
 void alg(Robot &robot) {
     show_img_debug debug;
-    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-        debug = show_debug_img;
-    #else
-        debug = save_debug_img;
-    #endif
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    debug = show_debug_img;
+#else
+    debug = save_debug_img;
+#endif
+    robot.WayFromFrame();
     double frame_offset = out_way_offset;
     robot.Go2({{0, out_way_offset}});
     RobotPoint start_position;
@@ -367,8 +394,8 @@ void alg(Robot &robot) {
         start_position = detect_position(robot, lidar_data, frame_offset);
     }
     write_log("Position:/n x: " + std::to_string(start_position.get_x()) +
-              " y: " + std::to_string(start_position.get_y()) +
-              " ang: " + std::to_string(start_position.get_angle()));
+        " y: " + std::to_string(start_position.get_y()) +
+        " ang: " + std::to_string(start_position.get_angle()));
     Point start_frame_point = Point{frame_offset * cos(start_position.get_angle()),
                                     frame_offset * sin(start_position.get_angle())} +
         start_position;
