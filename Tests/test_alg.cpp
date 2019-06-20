f1af541eabd765066a8629270f5e866d2c74c820
DanilaG <dndv279@gmail.com>
2019-06-20 21:52:37 +1000

    Распознание кубов.

--- Tests/test_alg.cpp
+++ Tests/test_alg.cpp
@@ -4,10 +4,26 @@
 
 #include "test.h"
 #include "../highLogic/alg.h"
+#include "../highLogic/CV.h"
 
 TEST(TestDetectPosition, 1) {
     Robot robot;
     std::vector<PolarPoint> points;
     ASSERT_FALSE(read("Real//8.ld", points));
     detect_position(robot, points, 300);
+}
+
+TEST(UpDateBox, 1) {
+    cv::Mat QRCodeImg;
+    std::array<BoxMap, 3> boxes;
+    std::pair<Point, Point> pz;
+    RobotPoint start_position = qr_detect(QRCodeImg, boxes, pz, "(M,F,N,H)(P,K,R,M)(F,H,D,J)(I,R,K,T)");
+    start_position.set_angle(0);
+    start_position.set_x(15 * field_sett::size_field_unit);
+    start_position.set_y(11 * field_sett::size_field_unit);
+    Map map(pz.first, pz.second, boxes, start_position);
+    cv::Mat img = map.get_img();
+    show_debug_img("Map", img);
+    Robot r;
+    update_box_color(r, map);
 }
--- highLogic/alg.h
+++ highLogic/alg.h
@@ -8,6 +8,7 @@
 #include "debug.h"
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
     #include "../wro2019main/Robot_low_Lvl_lib/Robot_facing.h"
+#include "map.h"
 #else
     #include "../wro2019main/Robot_low_Lvl_lib/Robot.h"
 #endif
@@ -16,6 +17,7 @@ const double out_way_offset = 300;
 
 void frame_connect(Robot &robot, double out_way_offset, double start_angle);
 RobotPoint detect_position(Robot &robot, std::vector<PolarPoint> &lidar_data, double frame_offset);
+void update_box_color(Robot &robot, Map &map);
 
 void do_alg_code(Robot &robot, bool kamikaze_mode = false, std::string s = "");
 void alg(Robot &robot);
